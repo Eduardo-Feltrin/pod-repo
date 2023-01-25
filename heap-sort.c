ca5be18d4dc0d2fc4a6dcddb921c969f6360be61
Double-Du <79672249+Eduardo-Feltrin@users.noreply.github.com>
2023-01-25 13:20:56 -0300

    Primeira tentativa de correção

--- heap-sort.c
+++ heap-sort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define heapSize 5;
 
 void maxHeapify(int a[], int i) {
   int aux = 0;
@@ -6,14 +7,14 @@ void maxHeapify(int a[], int i) {
   int leftChild = i << 1;
   int rightChild = leftChild + 1;
 
-  if (leftChild <= sizeof(a) / 4 &&
+  if (leftChild <= heapSize &&
       a[leftChild] > a[i]) { // sizeof recebe o tamanho do vetor e após isso
                              // divide por 4 para saber o número de elementos
     maior = leftChild;
   } else {
     maior = i;
   }
-  if (rightChild <= sizeof(a) / 4 && a[rightChild] > a[maior]) {
+  if (rightChild <= heapSize && a[rightChild] > a[maior]) {
     maior = rightChild;
   }
   if (maior != i) {
@@ -25,13 +26,12 @@ void maxHeapify(int a[], int i) {
 }
 
 void buildMaxHeap(int a[]) {
-  for (int i = (sizeof(a) / 4) / 2; i > 0; i--) {
+  for (int i = heapSize / 2; i > 0; i--) {
     maxHeapify(a, i);
   }
 }
 
 void heapSort(int a[]) {
-  int heapSize = sizeof(a) / 4;
   int aux = 0;
   buildMaxHeap(a);
   for (int i = heapSize; i > 1; i--) {
@@ -47,7 +47,6 @@ int main() {
   // Declaração de variáveis
   int a[] = {10, 20, 8, 4, 5};
   int i = 0;
-  int heapSize = sizeof(a) / 4;
 
   // Impressão dos valores presentes no vetor DESORDENADO
   printf("\n\tMétodo de ordenação 'Heap Sort'\n");
